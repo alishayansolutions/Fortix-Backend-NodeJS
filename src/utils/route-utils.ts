26b31102a2e48e224b199fc41a2d549ca4fe9c1e
alishayansolutions <ali.shayansolutions@gmail.com>
2025-01-03 11:16:42 +0500

    Fix: Error Handling

--- src/utils/route-utils.ts
+++ src/utils/route-utils.ts
@@ -10,6 +10,7 @@ export const routeHandler = (controller: ControllerFunction) => {
     try {
       await controller(req, res);
     } catch (error: any) {
+      console.log("Controller Error: ", error);
       if (error.statusCode) {
         const [errorResponse, status] = ResponseUtils.error(error.message, error.statusCode);
         res.status(status).json(errorResponse);
@@ -20,11 +21,10 @@ export const routeHandler = (controller: ControllerFunction) => {
         const [errorResponse, status] = ResponseUtils.error(MESSAGES.ERROR.RESOURCE_EXISTS, 409);
         res.status(status).json(errorResponse);
       } else {
-        console.error('Unhandled error:', error);
         const [errorResponse, status] = ResponseUtils.error(
           MESSAGES.ERROR.SERVER_ERROR,
           500,
-          process.env.NODE_ENV === 'development' ? error : undefined
+          error?.message
         );
         res.status(status).json(errorResponse);
       }
